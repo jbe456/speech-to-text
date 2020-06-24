7efb8dc3275fe82b990b421f002f9f5f648b1adf
Julien Bec <4415696+jbe456@users.noreply.github.com>
2020-06-24 22:07:51 +0200

    deploy website

---
+++ docs/precache-manifest.df659befc725829f1d14099f4f43e6b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c136de2c91f84085f23321596b506fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b093208895b03b5b81e0",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "48a452a234e9e8998f9c",
+    "url": "/static/css/main.6d6b6195.chunk.css"
+  },
+  {
+    "revision": "b093208895b03b5b81e0",
+    "url": "/static/js/2.d2aa60dd.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.d2aa60dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a452a234e9e8998f9c",
+    "url": "/static/js/main.af16ee8e.chunk.js"
+  },
+  {
+    "revision": "b7cda7596006b86e227b",
+    "url": "/static/js/runtime-main.c6b14e90.js"
+  }
+]);
