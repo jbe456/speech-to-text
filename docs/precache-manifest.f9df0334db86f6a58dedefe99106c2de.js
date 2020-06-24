73f6389ced48bdf289e7e2753af8ddc0063ec926
Julien Bec <4415696+jbe456@users.noreply.github.com>
2020-06-24 22:18:51 +0200

    fix deployed path

---
+++ docs/precache-manifest.f9df0334db86f6a58dedefe99106c2de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9b8ac15b378ea50b6a878fe5c30a67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6eebf5af1091b7468ff1",
+    "url": "./static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "faaf1d39cfbb746a4efd",
+    "url": "./static/css/main.6d6b6195.chunk.css"
+  },
+  {
+    "revision": "6eebf5af1091b7468ff1",
+    "url": "./static/js/2.e491c952.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "./static/js/2.e491c952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faaf1d39cfbb746a4efd",
+    "url": "./static/js/main.80ef1833.chunk.js"
+  },
+  {
+    "revision": "a3ca24fad5820c09e5fe",
+    "url": "./static/js/runtime-main.b3a48810.js"
+  }
+]);
